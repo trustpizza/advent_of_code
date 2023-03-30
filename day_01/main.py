4849e56562d628c7cbc18308b6868a38b0f215db
trustpizza <maxelolsson@gmail.com>
2023-03-30 15:01:56 -0500

    Setup

---
+++ day_01/main.py
@@ -0,0 +1,13 @@
+def part_one(filename: str) -> int:
+    calories = process_calories(filename)
+    return calories
+
+def part_two():
+    return
+
+def process_calories(filename: str) -> list[list[int]]:
+    return filename
+
+if __name__ == "__main__":
+    input_path = "./day_1/input.txt"
+    print(part_one(input_path))
