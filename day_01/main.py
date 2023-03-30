fbd603aa50f64044222dc3ee8a28d43c6f535655
trustpizza <maxelolsson@gmail.com>
2023-03-30 15:25:04 -0500

    Get file opening to work

--- day_01/main.py
+++ day_01/main.py
@@ -1,13 +1,27 @@
+import os
+
 def part_one(filename: str) -> int:
-    calories = process_calories(filename)
-    return calories
+    elf_calories = process_calories(filename)
+    return elf_calories
 
 def part_two():
     return
 
 def process_calories(filename: str) -> list[list[int]]:
-    return filename
+    file = read_file(filename)
+    #with open(filename, encoding="utf-8") as f:
+        #elves = f.read().split("\n\n")
+    return
+
+def read_file(filename):
+    file_handle = open(filename)
+    print(file_handle.read())
+    file_handle.close()
 
 if __name__ == "__main__":
-    input_path = "./day_1/input.txt"
-    print(part_one(input_path))
+    file_dir = os.path.dirname(os.path.realpath("__file__"))
+    input_file = "input.txt"
+    file_name = os.path.join(file_dir, input_file)
+    #print("/home/axel/advent_of_code/day_01/input.txt")
+    #print("---Part One---")
+    print(part_one(file_name))
