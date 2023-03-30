ae4cfc273f0a51d11b9ecde4c6eb7096f5b0105e
trustpizza <maxelolsson@gmail.com>
2023-03-30 15:31:43 -0500

    Add some new test data

--- day_01/main.py
+++ day_01/main.py
@@ -1,27 +1,21 @@
 import os
 
-def part_one(filename: str) -> int:
-    elf_calories = process_calories(filename)
+def part_one(file_name: str) -> int:
+    elf_calories = process_calories(file_name)
     return elf_calories
 
 def part_two():
     return
 
-def process_calories(filename: str) -> list[list[int]]:
-    file = read_file(filename)
-    #with open(filename, encoding="utf-8") as f:
-        #elves = f.read().split("\n\n")
+def process_calories(file_name: str) -> list[list[int]]:
+    with open(file_name, encoding="UTF-8") as f:
+        elves = f.read().split("\n\n")
+        print(elves)
     return
-
-def read_file(filename):
-    file_handle = open(filename)
-    print(file_handle.read())
-    file_handle.close()
 
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.realpath("__file__"))
-    input_file = "input.txt"
+    input_file = "temp.txt" #temp is the small test file
     file_name = os.path.join(file_dir, input_file)
-    #print("/home/axel/advent_of_code/day_01/input.txt")
-    #print("---Part One---")
+    print("---Part One---")
     print(part_one(file_name))
