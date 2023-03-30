65d32d00d1d704ad35b942c93c62df8b31e9f633
trustpizza <maxelolsson@gmail.com>
2023-03-30 15:45:35 -0500

    Find pretty solution for problem 1

--- day_01/main.py
+++ day_01/main.py
@@ -2,7 +2,11 @@ import os
 
 def part_one(file_name: str) -> int:
     elf_calories = process_calories(file_name)
-    return elf_calories
+    max = 0
+    for elf in elf_calories:
+        if max <= sum(elf):
+            max = sum(elf)
+    return max
 
 def part_two():
     return
@@ -10,12 +14,12 @@ def part_two():
 def process_calories(file_name: str) -> list[list[int]]:
     with open(file_name, encoding="UTF-8") as f:
         elves = f.read().split("\n\n")
-        print(elves)
-    return
+    return [list(map(int, elf.strip().split('\n'))) for elf in elves]
 
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.realpath("__file__"))
     input_file = "temp.txt" #temp is the small test file
     file_name = os.path.join(file_dir, input_file)
+
     print("---Part One---")
     print(part_one(file_name))
