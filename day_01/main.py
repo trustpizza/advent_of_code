8427f66bba7294d7bfcea49a220b1aba0d81812f
trustpizza <maxelolsson@gmail.com>
2023-03-30 15:55:33 -0500

    Add correct filepath

--- day_01/main.py
+++ day_01/main.py
@@ -22,7 +22,7 @@ def process_calories(file_name: str) -> list[list[int]]:
 
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.realpath("__file__"))
-    input_file = "temp.txt" #temp is the small test file
+    input_file = "input.txt" #temp is the small test file
     file_name = os.path.join(file_dir, input_file)
 
     print("---Part One---")
