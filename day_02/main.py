3badd56cd5206062e32f54b19161b8777e826ec6
trustpizza <maxelolsson@gmail.com>
2023-03-31 23:09:03 -0500

    Almost finished, too tired

--- day_02/main.py
+++ day_02/main.py
@@ -14,12 +14,38 @@ def part_one(file_name):
 
     return score
 
-def part_two():
-    return
+def part_two(file_name):
+    score = 0
+    combos = process_rps(file_name)
+    for game in combos:
+        game = list(game)
+        p1val = convert_value_to_rps(game[0])
+        p2val = find_next_value(p1val, game[1])
+
+        #print (p1val, p2val)
+        outcome = rocks_paper_scissors(p1val, p2val)
+        print(p1val)
+        score += p2val
+        score += outcome
+    return score
 
 # 1 is Rock
 # 2 is Paper
 # 3 is Scissors
+
+def find_next_value(val, goal):
+    out = None
+    if goal == "X":
+        if val == 1: out = 2
+        if val == 2: out = 3
+        if val == 3: out = 1
+    elif goal == "Z":
+        if val == 1: out = 3
+        if val == 2: out = 1
+        if val == 3: out = 2
+    else: out = val
+
+    return out
 
 def rocks_paper_scissors(val1, val2):
     if (val1 == val2):
@@ -59,12 +85,9 @@ def process_rps(file_name):
 
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.realpath("__file__"))
-    input_file = "input.txt" #temp is the small test file
-    
+    input_file = "temp.txt" #temp is the small test file
     file_name = os.path.join(file_dir, input_file)
-    print(file_name)
-
     print("---Part One---")
     print(part_one(file_name))
-    #print("---Part Two---")
-    #print(part_two(file_name))
+    print("---Part Two---")
+    print(part_two(file_name))
