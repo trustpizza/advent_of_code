bf5a2317f33c38fef55d03de46672b81edc8a78e
trustpizza <maxelolsson@gmail.com>
2023-03-31 20:39:20 -0500

    Return an array of pairs of letters

--- day_02/main.py
+++ day_02/main.py
@@ -1,13 +1,19 @@
 import os
 
 def part_one(file_name):
-    with open(file_name) as file:
-        x = file.read()
-        print(x)
+    print(process_rps(file_name))
     return
 
 def part_two():
     return
+
+def process_rps(file_name):
+    with open(file_name) as f:
+        combos = f.read()
+    combos = combos.split()
+    "".join(combos)
+
+    return ["".join(x) for x in zip(combos[0::2], combos[1::2])]
 
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.realpath("__file__"))
