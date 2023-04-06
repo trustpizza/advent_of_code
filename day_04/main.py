f01a3a6668090c477ec8f0c290ff46eeda9e7aed
trustpizza <maxelolsson@gmail.com>
2023-04-06 08:35:35 -0500

    Pseudocode for problem 1

--- day_04/main.py
+++ day_04/main.py
@@ -1,7 +1,17 @@
 
 
 def part_one(file_path):
-    open_file(file_path)
+    # Set a counter to 0
+    # Take the file input and split it one each line
+        # Take ech line and split it on the comma > this is your 2 elf pair 
+            # Each elf can be represented as an array of length elf[1] - elf[0]
+            # Take that elf and add every number between (and including) elf[0] and elf[1] to create an array of elf[1] - elf[0] length
+            # Do this for both elves
+        # Take the smaller elf (the larger elf can NEVER be fully contained by the other elf)
+            # Test to see if all of it's values exist in the larger elf
+                # If true, coutner ++ 
+                # Else, next elf pair
+    # Return counter
     return
 
 def part_two(file_path):
