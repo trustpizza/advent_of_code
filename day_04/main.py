35987aab35fa61c5e9435f121048a879e7a949b5
trustpizza <maxelolsson@gmail.com>
2023-04-06 11:13:34 -0500

    Fix up stinky method

--- day_04/main.py
+++ day_04/main.py
@@ -1,4 +1,4 @@
-
+import re
 
 def part_one(file_path):
     # Set a counter to 0
@@ -20,10 +20,25 @@ def part_two(file_path):
     return
 
 def open_file(file_path):
+    assignments = []
     with open(file_path) as f:
-        elves = f.read().split()
-    
-    return [list(map(list, elf)) for elf in elves] 
+        lines = f.read().strip().split()
+        for line in lines:
+            a,b,c,d = map(int, re.findall(r"\d", line))
+            assignments.append([[a,b],[c,d]])
+
+        # lines = f.read().strip().split()
+        # for temp_line in lines:
+        #     temp_line = temp_line.split(",")
+        #     temp_line = [list(val) for val in temp_line]
+        #     line = []
+        #     for elf in temp_line:
+        #         elf.remove("-")
+        #         elf = [eval(num) for num in elf]
+        #         line.append(elf)
+        #     assignments.append(line)
+    # THis is an ugly way to do this, but I can't find a better since whenever I map the elves, it resets it to a string
+    return assignments
 
 if __name__ == "__main__":
     input_file = "temp.txt" #temp is the small test file
