98a47b2b4b5b7f318a42722f5e2ec3fcec54d720
trustpizza <maxelolsson@gmail.com>
2023-04-06 11:44:25 -0500

    finish part 2 (that was easy)

--- day_04/main.py
+++ day_04/main.py
@@ -4,17 +4,6 @@ def get_diff(pair):
     return pair[1] - pair[0]
 
 def part_one(file_path):
-    # Set a counter to 0
-    # Take the file input and split it one each line
-        # Take ech line and split it on the comma > this is your 2 elf pair 
-            # Each elf can be represented as an array of length elf[1] - elf[0]
-            # Take that elf and add every number between (and including) elf[0] and elf[1] to create an array of elf[1] - elf[0] length
-            # Do this for both elves
-        # Take the smaller elf (the larger elf can NEVER be fully contained by the other elf)
-            # Test to see if all of it's values exist in the larger elf
-                # If true, coutner ++ 
-                # Else, next elf pair
-    # Return counter
     counter = 0
     assignments = parse_data(file_path)
     for pair in assignments:
@@ -29,6 +18,19 @@ def part_one(file_path):
     return counter
 
 def part_two(file_path):
+    counter = 0
+    assignments = parse_data(file_path)
+    for pair in assignments:
+        larger_pair = pair[0] if get_diff(pair[0]) > get_diff(pair[1]) else pair[1] # if diff pair[0][1] - pair[0][0] > pair[1][1] - pair[]
+        smaller_pair = pair[0] if get_diff(pair[0]) <= get_diff(pair[1]) else pair[1]
+
+        range_of_smaller = list(range(smaller_pair[0], smaller_pair[1] + 1))
+        
+        check = any(larger_pair[0] <= item <= larger_pair[1] for item in range_of_smaller)
+
+        if check: counter += 1
+    return counter
+
     return
 
 def parse_data(file_path):
