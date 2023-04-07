e88f9def34b320199844418bc9943151e3d1611b
trustpizza <maxelolsson@gmail.com>
2023-04-07 10:08:59 -0500

    Add data and skeletal structure

---
+++ day_05/main.py
@@ -0,0 +1,15 @@
+def part_one(input_file):
+    return
+
+def part_two(input_file):
+    return
+
+def parse_data(input_file):
+    return
+
+if __name__ == "__main__":
+    input_file = "temp.txt" #temp is the small test file
+    print("---Part One---")
+    print(part_one(input_file))
+    print("---Part Two---")
+    print(part_two(input_file)) 
