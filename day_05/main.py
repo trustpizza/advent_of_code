2e4724f783bb4308fb95acb5c7e3cbfe4547ecb0
trustpizza <maxelolsson@gmail.com>
2023-04-07 12:41:38 -0500

    Finish part 1 with test data

--- day_05/main.py
+++ day_05/main.py
@@ -1,11 +1,56 @@
-import re
-
 def part_one(input_file):
-    print(parse_data(input_file))
-    return
+    input_data = parse_data(input_file)
+    container_group = input_data[0]
+    moves = input_data[1]
+
+    for move in moves:
+        how_many = move[0]
+        start = move[1] - 1
+        destination = move[2] - 1
+
+        containers_moved = container_group[start][0:how_many]
+        for container in containers_moved:
+            container_group[destination].insert(0,container)
+            # container_group[destination].append(container)
+            #print("Moving:" + container_group[start][0:how_many] + "From:" + container_group[start] + "To:" + container_group[destination] + "Currently:" + container_group + "")
+        del container_group[start][0:how_many]
+
+    first_of_each_container_group = []
+
+    for stack in container_group:
+        first_of_each_container_group.append(stack[0])
+        # print(group[0])
+    return first_of_each_container_group
 
 def part_two(input_file):
     return
+
+def get_data_shape(container_data):
+    data_shape = []
+    col_nums = container_data[-1]
+    for i, num in enumerate(col_nums):
+        if num != " ":    
+            col = []
+            for line in container_data[:-1]:
+                if line[i] != " ": 
+                    col.append(line[i]) 
+            data_shape.append(col)
+    return data_shape
+
+def clean_move(move):
+    cleaned_move = []
+    move = move.split()
+    cleaned_move.append(int(move[1]))
+    cleaned_move.append(int(move[3]))
+    cleaned_move.append(int(move[5]))
+
+    return cleaned_move
+
+def get_moves(move_lines):
+    moves = []
+    for move in move_lines:
+        moves.append(clean_move(move))
+    return moves
 
 def parse_data(input_file):
     # Take the input file
@@ -17,24 +62,20 @@ def parse_data(input_file):
 
     # How to read the data
         # The last row, the one with columns has an identical number of spaces to an empty list.  
-    data_lines = []
-    data_shape = {}
     # Data shape is the 
-    moves = []
     with open(input_file) as f:
         file_lines = f.read().split("\n\n")
+        data_lines = []
+        move_lines = []
         for line in file_lines[0].splitlines():
             data_lines.append([*line])
-    col_nums = data_lines[-1]
-    for i, num in enumerate(col_nums):
-        if num != " ":    
-            col = []
-            for line in data_lines[:-1]:
-                if line[i] != " ": 
-                    col.append(line[i]) 
-            data_shape[int(num)] = col
-    print(data_shape)
-    return #[data_shape, moves]
+        for line in file_lines[1].splitlines():
+            move_lines.append(line)
+
+    data_shape = get_data_shape(data_lines)
+    moves = get_moves(move_lines)
+    
+    return [data_shape, moves]
 
 def test(input_file):
     with open(input_file) as f:
