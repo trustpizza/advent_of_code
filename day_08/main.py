4cf82ebc08e25578c6abf066ca2e46fc02e5d715
trustpizza <maxelolsson@gmail.com>
2023-04-11 18:31:14 -0500

    Transitioning to day 8

---
+++ day_08/main.py
@@ -0,0 +1,12 @@
+def part_one(input_file):
+    return
+
+def part_two(input_file):
+    return
+
+if __name__ == "__main__":
+    input_file = "temp.txt"
+    print("---Part One---")
+    print(part_one(input_file))
+    print("---Part Two---")
+    print(part_two(input_file)) 
