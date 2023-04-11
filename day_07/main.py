6078c4cd44d209f3d8c7c899e9873db4716c51f0
trustpizza <maxelolsson@gmail.com>
2023-04-10 22:36:32 -0500

    Can't figure out day 7 yet'

--- day_07/main.py
+++ day_07/main.py
@@ -1,7 +1,12 @@
+class Directory:
+    def __init__(self) -> None:
+        pass
+
 class File:
     def __init__(self, file_string):
         self.name = file_string.split(" ")[1]
         self.size = file_string.split(" ")[0]
+
 
 def part_one(input_file):
     # Split lines
@@ -15,8 +20,23 @@ def part_two(input_file):
 
 def group_commands(input_file):
     with open(input_file) as f:
-        f.read().splitlines()
-    return f
+        # lines = f.read().split("\n")
+        terminal_data = f.read().strip().split("\n")
+        counter = 0
+        # Take all the lines
+            # Take a counter
+            # Take each line and create a sublist of items between counter and index
+            # Look forward UNTIL 
+        # for idx, line in enumerate(lines):
+        #     print(counter)
+        #     if "cd" in line:
+        #         return
+                # print(counter,idx, line)
+                # counter = idx
+
+        # print(lines)
+    
+    
 
 if __name__ == "__main__":
     input_file = "temp.txt"
