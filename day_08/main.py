0b2bf6683cc5dd0453573dc71c9ac3e682dc138a
trustpizza <maxelolsson@gmail.com>
2023-04-12 13:15:36 -0500

    Start to plan columns

--- day_08/main.py
+++ day_08/main.py
@@ -1,3 +1,31 @@
+def unique_values(old_list) -> list:
+    new_list = []
+    for item in old_list:
+        if item not in new_list:
+            new_list.append(item)
+    return new_list
+    
+def get_visible_trees_from_reversed_list(trees) -> list:
+    trees = (list(reversed(trees)))
+    current_highest = -1
+    visible_trees = []
+    for tree_idx in range(len(trees)):
+        if trees[tree_idx] > current_highest:
+            visible_trees.append(tree_idx)
+            current_highest = trees[tree_idx]
+
+    visible_trees = list(map(lambda tree: abs(tree - 4), visible_trees))
+    return visible_trees
+
+def get_visible_trees_from_list(trees) -> list:
+    current_highest = -1
+    visible_trees = []
+    for tree_idx in range(len(trees)):
+        if trees[tree_idx] > current_highest:
+            visible_trees.append(tree_idx) #technically the index of the tree
+            current_highest = trees[tree_idx]
+    return visible_trees
+
 def get_col_from_list(list) -> list:
     cols = []
     for row_idx in range(len(list)):
@@ -6,6 +34,7 @@ def get_col_from_list(list) -> list:
             col.append(list[col_idx][row_idx])
         cols.append(col)
     return cols
+
 def get_perimeter_trees(forest) -> list:
     # Take the first row, the last row, the first col, and the last col
     perimeter_trees_locations = []
@@ -23,27 +52,40 @@ def get_internal_trees(forest) -> list:
     for row_idx, row in enumerate(forest):
         col = cols[row_idx]
         # First take rows, split each row in half by it's largest value
-        for col_idx in range(len(row)):
-            loc = [row_idx,col_idx]
-            highest_tree_idx = row.index(max(row))
-            # Rows First
-            left_row_half = row[0:highest_tree_idx]
-            right_row_half = row[highest_tree_idx:None]
-            # Cols Second
-            
-            # print(left_row_half, right_row_half)
+        highest_tree_idx_row = row.index(max(row))
+        highest_tree_idx_col = col.index(max(col))
 
+        # Split rows
+        left_row_half = row[0:highest_tree_idx_row+1]
+        right_row_half = row[highest_tree_idx_row:None]
+        # Split Cols
+        top_col_half = col[0:highest_tree_idx_col+1]
+        bottom_col_half = col[highest_tree_idx_col:None]
+        
+        print(top_col_half, bottom_col_half)
+
+
+        visible_left = get_visible_trees_from_list(left_row_half)
+        visible_right = get_visible_trees_from_reversed_list(right_row_half)
+        # print(left_row_half)
+        for tree in visible_left:
+            internal_tree_locations.append([row_idx,tree])
+        for tree in visible_right:
+            internal_tree_locations.append([row_idx, tree])
+
+    # internal_tree_locations = unique_values(internal_tree_locations)
     return internal_tree_locations
 
 def part_one(input_file):
     data = parse_input(input_file)
-    visible_trees = 0
+    visible_trees_count = 0
     # Step 1, get all perimeter values
     perimeter_trees = get_perimeter_trees(data)
     internal_trees = get_internal_trees(data)
-    # print(internal_trees)
+    visible_trees =  unique_values(perimeter_trees+internal_trees)
+    print(len(visible_trees))
     # visible_trees += len(perimeter_trees)
-    return visible_trees
+    return visible_trees_count
 
 
 def part_two(input_file):
