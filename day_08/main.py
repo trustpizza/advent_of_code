d9ddae85085da58d20f49520c418d193ffa513f6
trustpizza <maxelolsson@gmail.com>
2023-04-12 12:23:34 -0500

    Work on splitting problem into tinier problems

--- day_08/main.py
+++ day_08/main.py
@@ -1,8 +1,49 @@
+def get_col_from_list(list) -> list:
+    cols = []
+    for row_idx in range(len(list)):
+        col = []
+        for col_idx in range(len(list[0])):
+            col.append(list[col_idx][row_idx])
+        cols.append(col)
+    return cols
+def get_perimeter_trees(forest) -> list:
+    # Take the first row, the last row, the first col, and the last col
+    perimeter_trees_locations = []
+    # Get the location of each tree
+    for idx, row in enumerate(forest):
+        for col in range(len(row)):
+            loc = [idx, col]
+            if idx == 0 or idx == len(row)-1 or col == 0 or col == len(row) - 1:
+                perimeter_trees_locations.append(loc)
+    return perimeter_trees_locations
+
+def get_internal_trees(forest) -> list:
+    internal_tree_locations = []
+    cols = get_col_from_list(forest)
+    for row_idx, row in enumerate(forest):
+        col = cols[row_idx]
+        # First take rows, split each row in half by it's largest value
+        for col_idx in range(len(row)):
+            loc = [row_idx,col_idx]
+            highest_tree_idx = row.index(max(row))
+            # Rows First
+            left_row_half = row[0:highest_tree_idx]
+            right_row_half = row[highest_tree_idx:None]
+            # Cols Second
+            
+            # print(left_row_half, right_row_half)
+
+    return internal_tree_locations
 
 def part_one(input_file):
     data = parse_input(input_file)
-
-    return data
+    visible_trees = 0
+    # Step 1, get all perimeter values
+    perimeter_trees = get_perimeter_trees(data)
+    internal_trees = get_internal_trees(data)
+    # print(internal_trees)
+    # visible_trees += len(perimeter_trees)
+    return visible_trees
 
 
 def part_two(input_file):
