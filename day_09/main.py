624b5994f196ad8a80c7392280c8b67d8f40cbd8
trustpizza <maxelolsson@gmail.com>
2023-04-14 08:50:18 -0500

    Write really clean code to find the up and down, left and right inputs

--- day_09/main.py
+++ day_09/main.py
@@ -1,6 +1,19 @@
+def determine_width(lines):
+    pass
+
+def determine_height(lines):
+    pass
+
+def find_list_size(lines):
+    up_and_down_inputs = list(filter(lambda item: item is not None, map(lambda line: line if (line[0] == "U" or line[0] == "D") else None, lines)))
+    left_and_right_inputs = list(filter(lambda item: item is not None, map(lambda line: line if line[0] == "R" or line[0] == "L" else None, lines)))
+    print(up_and_down_inputs)
+    print(left_and_right_inputs)
+
 def part_one(file):
     data = parse_inputs(file)
-    print(data)
+    find_list_size(data)
+    return data
 
 def part_two(file):
     return
