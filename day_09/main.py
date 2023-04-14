4e28bd4dc31f2bbe10103852342b527b7c10bf45
trustpizza <maxelolsson@gmail.com>
2023-04-13 20:15:26 -0500

    add code skeleton

---
+++ day_09/main.py
@@ -0,0 +1,12 @@
+def part_one(file):
+    pass
+
+def part_two(file):
+    pass
+
+if __name__ == "__main__":
+    input_file = "input.txt"
+    print("---Part One---")
+    print(part_one(input_file))
+    print("---Part Two---")
+    print(part_one(input_file))
