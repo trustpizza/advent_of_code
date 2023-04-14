fdabbd8e39a8fdcb0d2ebe54811b93ebed06b232
trustpizza <maxelolsson@gmail.com>
2023-04-14 08:36:17 -0500

    Write parse_inputs method to return a list with a Letter and a number

--- day_09/main.py
+++ day_09/main.py
@@ -1,12 +1,24 @@
 def part_one(file):
-    pass
+    data = parse_inputs(file)
+    print(data)
 
 def part_two(file):
-    pass
+    return
+
+def clean_input_line(line) -> None:
+    line = line.split(" ")
+    line[1] = int(line[1])
+    return line
+
+def parse_inputs(file):
+    with open(file) as f:
+        lines = f.read().splitlines()
+        lines = list(map(clean_input_line, lines))
+    return lines
 
 if __name__ == "__main__":
-    input_file = "input.txt"
+    input_file = "temp.txt"
     print("---Part One---")
     print(part_one(input_file))
     print("---Part Two---")
-    print(part_one(input_file))
+    print(part_two(input_file))
