731f5ba0fbe78b53f439422d34692d1790aed1f4
trustpizza <maxelolsson@gmail.com>
2023-04-15 13:55:59 -0500

    Remove board

--- day_09/main.py
+++ day_09/main.py
@@ -1,15 +1,3 @@
-class RopeBridge:
-    def __init__(self, lines) -> None:
-        self.bridge = find_list_size(lines)
-        self.tail = self.bridge[0][0] = "T"
-        self.head = self.bridge[0][0] = "H"
-
-    def show_bridge(self):
-        print(self.bridge)
-
-    def move_head(direction, spaces):
-        pass
-
 def determine_max(lines):
     max_location = 0
     current_location = 0
@@ -22,23 +10,25 @@ def determine_max(lines):
         if current_location > max_location: max_location = current_location
     return max_location
 
-def find_list_size(lines):
-    up_and_down_inputs = list(filter(lambda item: item is not None, map(lambda line: line if (line[0] == "U" or line[0] == "D") else None, lines)))
-    left_and_right_inputs = list(filter(lambda item: item is not None, map(lambda line: line if line[0] == "R" or line[0] == "L" else None, lines)))
+# def find_list_size(lines):
+#     up_and_down_inputs = list(filter(lambda item: item is not None, map(lambda line: line if (line[0] == "U" or line[0] == "D") else None, lines)))
+#     left_and_right_inputs = list(filter(lambda item: item is not None, map(lambda line: line if line[0] == "R" or line[0] == "L" else None, lines)))
     
-    # print(up_and_down_inputs)
-    max_height = determine_max(up_and_down_inputs)
-    # print(max_height)
-    # print(left_and_right_inputs)
-    max_width = determine_max(left_and_right_inputs)
+#     # print(up_and_down_inputs)
+#     # print(max_height)
+#     # print(left_and_right_inputs)
 
-    return [[None for i in range(max_width)] for i in range(max_height)]# * max_height
+#     return [[" " for i in range(max_width)] for i in range(max_height)]# * max_height
 
 def part_one(file):
     data = parse_inputs(file)
-    bridge = RopeBridge(data)
+    # bridge = RopeBridge(data)
+    
+    # for line in data:
+        # bridge.process_movement(line)
+    # bridge.show_bridge()
 
-    return bridge
+    return 
 
 def part_two(file):
     return
