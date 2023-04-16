75867b36faf7b26898e35a41f7b9b614ab99e928
trustpizza <maxelolsson@gmail.com>
2023-04-16 11:20:36 -0500

    Add child to knots, close to getting it working

--- day_09/main.py
+++ day_09/main.py
@@ -1,7 +1,8 @@
 class Knot:
-    def __init__(self, location: list) -> None:
+    def __init__(self, location: list, child = None) -> None:
         self.location = location
         self.was_diag = False
+        self.child = child
 
     def move(self,direction) -> None:
         if direction == "U":
@@ -25,6 +26,9 @@ class Knot:
     def set_diag_true(self):
         self.was_diag = True
 
+    def set_child(self, child):
+        self.child = child
+
 def check_rope_is_touching(head_loc, tail_loc):
     if tail_loc[0] >= head_loc[0]-1 and tail_loc[0] <= head_loc[0] +1 and tail_loc[1] >= head_loc[1] -1 and tail_loc[1] <= head_loc[1] +1:
         return True
@@ -38,8 +42,8 @@ def is_diagnol(head_loc,tail_loc):
 def part_one(file):
     data = parse_inputs(file)
     
-    head = Knot([0,0])
     tail = Knot([0,0])
+    head = Knot([0,0], tail)
 
     locations = []
     # was_diag = False 
@@ -69,45 +73,76 @@ def part_two(file):
     data = parse_inputs(file)
 
     knots = []
-    for _ in range(3): # Create and populate Knots list
+    for _ in range(4): # Create and populate Knots list
         knot = Knot([0,0])
         knots.append(knot)
+
+    for i in range(len(knots)):
+        if i < len(knots)-1:
+            knots[i].set_child(knots[i+1])
 
     locations = []
 
     for line in data:
         for _ in range(line[1]):
-            for idx, knot in enumerate(knots):
-                if not idx == len(knots) -1: # If not the last one
-                    # print("Head")
-                    knot_loc = list(knot.get_loc())
-                    knot.move(line[0])
-                    relative_tail = knots[idx+1]
+            for i in range(len(knots)):
+                head = knots[i]
+                tail = head.child
+                if tail: 
+                    head_loc = list(head.get_loc())
+                    head.move(line[0])
 
-                    if relative_tail.was_diag and not check_rope_is_touching(knot.get_loc(), relative_tail.get_loc()):
-                        relative_tail.set_loc(knot_loc)
+                    if tail.was_diag and not check_rope_is_touching(head.get_loc(), tail.get_loc()): # Valid move
+                        tail.set_loc(head_loc)
+            
+                    tail.set_diag_false()
+
+                    if is_diagnol(head.get_loc(),tail.get_loc()): # Prepare for the next move
+                        tail.set_diag_true()  
+
+                    if not check_rope_is_touching(head.get_loc(),tail.get_loc()): # Move the tail for normal diaganols
+                        tail.move(line[0])
                     
-                    relative_tail.set_diag_false()
-
-                    if is_diagnol(knot.get_loc(), relative_tail.get_loc()):
-                        relative_tail.set_diag_true()
+                    if tail.child == None:
+                        locations.append(list(tail.get_loc()))
                     
-                    if not check_rope_is_touching(knot.get_loc(), relative_tail.get_loc()):
-                        relative_tail.move(line[0])
-
-                    if relative_tail == knots[-1]:
-                        print(knots[0].get_loc(),knot.get_loc(), relative_tail.get_loc())
-                elif idx == len(knots) -1:
-                    # print("Tail")
-                    # print(knot.get_loc())
+                    print(knots[0].get_loc(), knots[1].get_loc(), knots[2].get_loc(), knots[3].get_loc())
                     
-                    locations.append(list(knot.get_loc()))
+            # for idx, knot in enumerate(knots):
+            #     if not idx == len(knots) -1: # If not the last one
+            #         # ISSUE IS THAT THE TRUE HEAD IS NOT UPDATING IT'S POSITION!
+            #         knot_loc = list(knot.get_loc())
+            #         knot.move(line[0])
+            #         relative_tail = knots[idx+1]
+
+            #         if knot == knots[0]:
+            #             print(knot.get_loc(), relative_tail.get_loc())
+
+            #         if relative_tail.was_diag and not check_rope_is_touching(knot.get_loc(), relative_tail.get_loc()):
+            #             relative_tail.set_loc(knot_loc)
+                    
+            #         relative_tail.set_diag_false()
+
+            #         if is_diagnol(knot.get_loc(), relative_tail.get_loc()):
+            #             relative_tail.set_diag_true()
+                    
+            #         if not check_rope_is_touching(knot.get_loc(), relative_tail.get_loc()):
+            #             relative_tail.move(line[0])
+
+            #         if relative_tail == knots[-1]:
+            #             # print(knots[0].get_loc(),knot.get_loc(), relative_tail.get_loc())
+            #             pass
+            #     elif idx == len(knots) -1:
+            #         # print("Tail")
+            #         # print(knot.get_loc())
+                    
+            #         locations.append(list(knot.get_loc()))
 
 
                     
-                # if idx < 9:
-                #     relative_tail = knots[idx+1]
-                pass
+            #     # if idx < 9:
+            #     #     relative_tail = knots[idx+1]
+            #     pass
 
     out = set(tuple(loc) for loc in locations)
 
