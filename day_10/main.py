06de09656d85f138c58a8ffee072147b0b4c66ed
trustpizza <maxelolsson@gmail.com>
2023-04-18 13:09:08 -0500

    Write psuedocode for input processs

---
+++ day_10/main.py
@@ -0,0 +1,33 @@
+def part_one(file):
+    pass
+
+def part_two(file):
+    pass
+
+def proces_input(filename: str) -> dict[int,int]:
+    # Opent the file
+    # Have a cycle Count
+    # Have what x equals
+    # Dictionary = {}
+    # For _ in range of file length:
+        # Case Match:
+            # If Noop:
+                # Cycle+=1
+                # dict(cycle) = x
+            # if Addx:
+                # cycle +=1
+                # dict(cycle) = x
+                # cycle +=1
+                # x+= file_input
+                # dict(cycle) = x
+
+    # Return Dictionary        
+    pass
+
+if __name__ == "__main__":
+    input_path = "input.txt"
+    print("---Part One---")
+    print(part_one(input_path))
+
+    print("---Part Two---")
+    print(part_two(input_path))
