2127ae59496ae18a4030fbc9f114c13a6a0b87dc
trustpizza <maxelolsson@gmail.com>
2023-04-24 13:03:02 -0500

    Borrow other answer to solve problem faster

--- day_12/main.py
+++ day_12/main.py
@@ -1,87 +1,93 @@
-class Hike:
-    MOVES = [
-        (0,1),
-        (1,0),
-        (-1,0),
-        (0,-1)
-    ]
-
-    possible_outcomes = []
-
-    def __init__(self, heightmap, current_location, destination, prior_locations = []) -> object:
-        self.current_location = current_location
-        self.prior_locations = prior_locations
-        self.heightmap = heightmap
-        self.destination = destination
-        self.current_value = self.find_current_value()
+from collections import deque
+from typing import Deque
 
 
-    def find_current_value(self):
-        return self.heightmap[self.current_location[0]][self.current_location[1]]
+def part_one(filename: str) -> int:
+    lines = parse_input(filename)
+    start, end = get_start_end(lines)
+    graph = construct_graph(lines)
+    return bfs(graph, start, end)
 
-    def move(self):
-        self.check_hike_over()
 
-        next_locations = []
-        for move in Hike.MOVES:
-            next_location = (self.current_location[0] + move[0], self.current_location[1] + move[1])
-            next_location = self.check_next_location(next_location)
-            if next_location is not None: next_locations.append(next_location)
-        
-        next_locations = list(map(lambda x: is_legal_move(self.current_value, x, self.heightmap), next_locations))
-        next_locations = list(filter(lambda x: x is not None, next_locations))
+def part_two(filename: str) -> int:
+    lines = parse_input(filename)
+    start, end = get_start_end(lines)
+    graph = construct_graph(lines)
+    start_candidates = {(x, y) for x, y in graph if get_elevation(lines[y][x]) == 0}
+    steps_required = []
+    for start in start_candidates:
+        steps = bfs(graph, start, end)
+        if steps != -1:
+            steps_required.append(steps)
+    return min(steps_required)
 
-        for next_move in next_locations:
-            prior_locations = list(self.prior_locations)
-            prior_locations.append(next_move)
 
-            next_hike = Hike(self.heightmap, next_move, self.destination, prior_locations)
+def parse_input(filename: str) -> list[str]:
+    with open(filename, encoding="utf8") as f:
+        lines = [line.strip() for line in f.readlines()]
+    return lines
 
-            next_hike.move()
-    
-    def check_next_location(self, next_location):
-        if (0 <= next_location[0] <= len(self.heightmap) -1) and (0 <= next_location[1] <= len(self.heightmap[0])-1) and (next_location not in self.prior_locations):
-            return next_location
-        else:
-            return 
 
-    def check_hike_over(self):
-        if self.current_location == self.destination:
-            Hike.possible_outcomes.append(self)
+def get_start_end(lines: list[str]) -> tuple[tuple[int, int], tuple[int, int]]:
+    start = end = (-1, -1)
+    for y, line in enumerate(lines):
+        if line.find("S") != -1:
+            start = (line.find("S"), y)
+        if line.find("E") != -1:
+            end = (line.find("E"), y)
+    return start, end
 
-def is_legal_move(current_value, move, heightmap):
-    if heightmap[move[0]][move[1]] in range(current_value - 1, current_value+2):
-        return move
 
-def part_one(file):
-    heightmap = parse_inputs(file)
-    for i, line in enumerate(heightmap):
-        for j,char in enumerate(line):
-            if char == 83:
-                start = (i,j)
-                heightmap[i][j] = 97
-            if char == 69:
-                destination = (i,j)
-                heightmap[i][j] = 122
+def get_elevation(elevation: str) -> int:
+    if elevation == "S":
+        return get_elevation("a")
+    elif elevation == "E":
+        return get_elevation("z")
+    return ord(elevation) - ord("a")
 
-    shortest_route = Hike(heightmap, start, destination)
-    shortest_route.move()
 
-    lens_outcomes = []
+def construct_graph(lines: list[str]) -> dict[tuple[int, int], set[tuple[int, int]]]:
+    graph = {}
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            coord = (x, y)
+            children = set()
+            candidates = [
+                (x + dx, y + dy)
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                if 0 <= x + dx < len(line) and 0 <= y + dy < len(lines)
+            ]
+            for x1, y1 in candidates:
+                if get_elevation(lines[y1][x1]) - get_elevation(char) <= 1:
+                    children.add((x1, y1))
+            graph[coord] = children
+    return graph
 
-    for route in Hike.possible_outcomes:
-        lens_outcomes.append(len(route.prior_locations))
 
-    return min(lens_outcomes)
+def bfs(
+    graph: dict[tuple[int, int], set[tuple[int, int]]],
+    start: tuple[int, int],
+    end: tuple[int, int],
+):
+    visited = {start: 0}
+    steps = 0
+    queue: Deque[tuple[tuple[int, int], int]] = deque()
+    queue.append((start, steps))
+    while queue:
+        current, steps = queue.popleft()
+        if current == end:
+            return steps
+        for neighbour in graph[current]:
+            if neighbour not in visited:
+                visited[neighbour] = steps + 1
+                queue.append((neighbour, steps + 1))
+    return -1
 
-def parse_inputs(file):
-    with open(file, encoding="utf8") as f:
-        lines = f.read().splitlines()
-    elevation_map = list(map(lambda line: list(map(lambda char: ord(char), line)),list([*line] for line in lines)))
-
-    return elevation_map  
 
 if __name__ == "__main__":
-    filename = "input.txt"
+    input_path = "input.txt"
     print("---Part One---")
-    print(part_one(filename))
+    print(part_one(input_path))
+
+    print("---Part Two---")
+    print(part_two(input_path))
