5ef6c459c0c92d002fcb9ff7f78146f666e120a8
trustpizza <maxelolsson@gmail.com>
2023-04-26 13:54:36 -0500

    Use ast to evaluate each line in line pairs

--- day_13/main.py
+++ day_13/main.py
@@ -1,12 +1,16 @@
+import ast
+
 def part_one(file):
     line_pairs = parse_inputs(file)
     return 
 
 def parse_inputs(file):
     with open(file, encoding="utf8") as f:
-        line_pairs = f.read().split("\n\n")
-        line_pairs = list(map(lambda pair: pair.split("\n"), line_pairs))
-    return line_pairs
+        return [
+            [ast.literal_eval(line) for line in pair.strip().split("\n")]
+            for pair in f.read().split("\n\n")
+        ]
+        
 
 if __name__ == "__main__":
     filename = "temp.txt"
