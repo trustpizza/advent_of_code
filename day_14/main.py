7c12da6a10c222bf1a4ba618bb858d9b5caef830
trustpizza <maxelolsson@gmail.com>
2023-04-28 11:14:53 -0500

    Create way to find rocks, but not perfect.  Needs to fix

--- day_14/main.py
+++ day_14/main.py
@@ -1,13 +1,33 @@
 def part_one(file):
     lines = parse_inputs(file)
-    print(lines)
+    for line in lines:
+        for idx in range(len(line)):
+            if idx < len(line) - 1:
+                find_rocks(line[idx], line[idx+1])
     # First draw rock positions
+    print(lines)
     pass
+
+
+def find_rocks(first_pos, second_pos):
+    if first_pos[0] - second_pos[0] == 0:
+        for i in range(abs(first_pos[1]-second_pos[1]) + 1):
+            rock = (first_pos[0], first_pos[1]+i)
+            print(rock)
+    else:
+        for i in range(abs(first_pos[0]-second_pos[0]) +1):
+            rock = (first_pos[0]+i, first_pos[1])
+            print(rock)
+    # Take a line
+        # Take the first two tuples
+        # Subtract the first items of both, if == 0, Rock is moving on the X axis
+            # Return Tuples STarting with the First, Ending with the Last, and with all in between
+        # Else the Rocks are moving on the Y axis
+    
 
 
 def parse_inputs(file):
     with open(file, "r", encoding="utf-8") as f:
-        # lines = f.read().splitlines()
         lines = list(map(lambda line: list(map(lambda entry: tuple(map(lambda input_half: int(input_half), entry.split(","))), line)), [line.split(" -> ") for line in f.read().splitlines()]))
     return lines
 
@@ -15,4 +35,4 @@ def parse_inputs(file):
 if __name__ == "__main__":
     filename = 'temp.txt'
     print("---Part One---")
-print(part_one(filename))
+    print(part_one(filename))
